df0049e8fd9adf6b811ae5a13e4d6ec6c929c6a3
Fabian <fabig4da@gmail.com>
2024-05-30 14:29:47 -0500

    set up shadcn

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button"
+import { Switch } from "@/components/ui/switch"
+
+
+function App() {
+
+  return (
+    <>
+     <Button>Click here</Button>
+     <Switch id="s" />
+    </>
+  )
+}
+
+export default App
