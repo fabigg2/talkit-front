30d3b88b5ddbda04238b1e08ba584feeeb884895
Fabian <fabig4da@gmail.com>
2024-06-09 21:02:50 -0500

    Layout home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { Button } from "@/components/ui/button"
 import { Switch } from "@/components/ui/switch"
 import { TextFiedBase } from "./components/form/TextField"
 import LogIn from "./auth/LogIn"
+import { HomePage } from "./layout/HomePage"
 
 
 function App() {
 
   return (
     <>
-     <LogIn />
+     <HomePage />
     </>
   )
 }
